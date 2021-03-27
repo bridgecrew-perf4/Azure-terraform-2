f0eedc7c7fd252638ef511453ea9d7c4b00a49e0
Lahiru Peramune <lahiru.peramune@ifsworld.com>
2021-03-27 20:34:46 +0530

    remove tfvars

--- Terraform/readme.txt
+++
@@ -1,2 +0,0 @@
-resource_group_name = "terrform-demo-rg1"
-location = "westeurope"
--- Terraform/variable.tf
+++ Terraform/variable.tf
@@ -3,6 +3,7 @@ variable "resource_group_name" {
    description = "name of the resource group"
    default = "default-rg"
 }
+
 variable "location" {
  type = string
  description = "location "
